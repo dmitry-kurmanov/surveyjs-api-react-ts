8967e24adea671addbb6370b60fdaf7551abc5a0
dmitry-kurmanov <kurmanov.work@gmail.com>
2024-05-24 16:06:40 +0400

    work for the root layout - 2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,17 @@
-export default function TopMenu() {
-    return <header>Top Menu</header>
-}
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../../state-container/store.ts';
+import {setLocale} from "../../state-container/slices/localeSlice.ts"
 
-{/* <header>
-<label>
+export default function Header() {
+  const currentLocale = useSelector((state:RootState) => state.locale.value);
+  const dispatch = useDispatch();
+
+    return <header>
+      <label>
   <select name="locale" defaultValue={currentLocale} onChange={e => dispatch(setLocale(e.target.value))}>
     <option value="en">EN</option>
     <option value="ru">RU</option>
   </select>
 </label>
-</header> */}
+    </header>
+}
--- src/routes/RootPage.tsx
+++ src/routes/RootPage.tsx
@@ -1,13 +1,6 @@
 import SurveysList from '../components/surveyList/SurveysList.tsx'
 
-import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from '../state-container/store.ts';
-import {setLocale} from "../state-container/slices/localeSlice.ts"
-
 function RootPage() {
-  const currentLocale = useSelector((state:RootState) => state.locale.value);
-  const dispatch = useDispatch();
-
   return <>
       <SurveysList />
   </>
