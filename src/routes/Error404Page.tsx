1ea8a3e340377536b66817d088a91da835fcaa87
dmitry-kurmanov <kurmanov.work@gmail.com>
2024-06-14 13:38:36 +0400

    format (prettier preparation)

--- src/routes/Error404Page.tsx
+++ src/routes/Error404Page.tsx
@@ -6,25 +6,25 @@ import { RootState } from "../state-container/store.ts";
 import { useSelector } from "react-redux";
 
 interface IRouteError {
-    statusText: string;
-    message: string;
+  statusText: string;
+  message: string;
 }
 
 export default function ErrorPage404({ customStatusText }: { customStatusText?: string }) {
-    const locale = useSelector((state:RootState) => state.settings.value.locale);
-    const {title, subTitle, goToHome} = getTexts(locale).errorPage;
+  const locale = useSelector((state: RootState) => state.settings.value.locale);
+  const { title, subTitle, goToHome } = getTexts(locale).errorPage;
 
-    const error:IRouteError = useRouteError() as IRouteError;
-    console.error(error);
+  const error: IRouteError = useRouteError() as IRouteError;
+  console.error(error);
 
-    return (
-        <div id="error-page">
-          <h1>{title}</h1>
-          <h2>{subTitle}</h2>
-          <p>
-            <i>{customStatusText || error.statusText || error.message}</i>
-          </p>
-          <Link to="/" component={RouterLink}>{goToHome}</Link>
-        </div>
-      );
+  return (
+    <div id="error-page">
+      <h1>{title}</h1>
+      <h2>{subTitle}</h2>
+      <p>
+        <i>{customStatusText || error.statusText || error.message}</i>
+      </p>
+      <Link to="/" component={RouterLink}>{goToHome}</Link>
+    </div>
+  );
 }
