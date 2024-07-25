f6066aba3faf9e16e8ba309579393e611925911d
dmitrykurmanov <kurmanov.work@gmail.com>
2024-07-25 10:24:31 +0400

    work for electron (added packager)

---
+++ dist/electron-main.js
@@ -0,0 +1,37 @@
+// Modules to control application life and create native browser window
+import { app, BrowserWindow } from "electron";
+import { createRequire } from "node:module";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const require = createRequire(import.meta.url);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // webPreferences: {
+    //   preload: path.join(__dirname, "preload.js"),
+    // },
+  });
+
+  win.loadFile("dist/index.html");
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
